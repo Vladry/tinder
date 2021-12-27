7aca42a647c74bb08ed781f6777542f3e1912477
УкрТехноСервис <rvy@ukr.net>
2021-12-28 01:03:45 +0200

    save

--- src/main/java/tinder/dao/UserJdbcDao.java
+++ src/main/java/tinder/dao/UserJdbcDao.java
@@ -1,42 +1,31 @@
 package tinder.dao;
 
-//import com.mysql.cj.jdbc.MysqlConnectionPoolDataSource;
-
 import org.postgresql.ds.PGPoolingDataSource;
 
+import com.zaxxer.hikari.HikariDataSource;
 import java.sql.*;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 public class UserJdbcDao implements UserDao  {
-//        private MysqlConnectionPoolDataSource source;
-    private PGPoolingDataSource source;
+
     List<User> allUsers = new ArrayList<>();
 
-/*    public UserJdbcDao() {
-        try {
-            source = new MysqlConnectionPoolDataSource();
-            source.setServerName("localhost");
-            source.setPort(3306);
-            source.setDatabaseName("users");
-            source.setUser("root");
-            source.setPassword("root");
-            source.setAllowMultiQueries(true);
+    public class UserJdbcDao implements UserDao {
 
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
+        private HikariDataSource source;
+        List<User> allUsers = new ArrayList<>();
+
+        public UserJdbcDao() {
+            source = new HikariDataSource();
+            source.setDriverClassName("org.postgresql.ds.PGSimpleDataSource");
+            source.setJdbcUrl("jdbc:postgresql://ec2-52-86-177-34.compute-1.amazonaws.com:5432/d7g10jrgsjruk4");
+            source.setUsername("mtmaprkfztrfne");
+            source.setPassword("d727d367387272970efb9ca62ff523bb77695ebf5f9a7e7b83af48e216e2fb64");
+            source.setMinimumIdle(1);
+            source.setMaximumPoolSize(3);
         }
-    }*/
 
-    public UserJdbcDao() {
-        source = new PGPoolingDataSource();
-        source.setServerName("ec2-52-86-177-34.compute-1.amazonaws.com");
-        source.setDatabaseName("d7g10jrgsjruk4");
-        source.setUser("mtmaprkfztrfne");
-        source.setPassword("d727d367387272970efb9ca62ff523bb77695ebf5f9a7e7b83af48e216e2fb64");
-        source.setMaxConnections(10);
-    }
 
     @Override
     public boolean create(User user) {
